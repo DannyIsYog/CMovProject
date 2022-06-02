c43d2581d7e4b656b190a9fe3a61a50b15377900
urbano2000 <beatriz.urbano@tecnico.ulisboa.pt>
2022-06-02 13:56:38 +0100

    bottom navigation and option of continue without login added

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/ChatPage.java
@@ -0,0 +1,20 @@
+package pt.ulisboa.tecnico.cmov.cmovproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ChatPage extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+
+    }
+
+}
+
+
--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/Login.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/Login.java
@@ -3,10 +3,12 @@ package pt.ulisboa.tecnico.cmov.cmovproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class Login extends AppCompatActivity {
     private EditText edtPassword ;
     private Button btnLogin;
     private Button btnSignUp;
+    private TextView btnNoLogIn;
 
 
 
@@ -27,11 +30,25 @@ public class Login extends AppCompatActivity {
         edtPassword = findViewById(R.id.edt_password);
         btnLogin = findViewById(R.id.btn_login);
         btnSignUp = findViewById(R.id.btn_signup);
+        btnNoLogIn= findViewById(R.id.btn_no_login);
+
+        btnNoLogIn.setPaintFlags(btnNoLogIn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 Intent intent = new Intent(Login.this,SingUp.class);
                 startActivity(intent);
+
+            }
+        });
+
+        btnNoLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent1 = new Intent(Login.this,ChatPage.class);
+                startActivity(intent1);
 
             }
         });
