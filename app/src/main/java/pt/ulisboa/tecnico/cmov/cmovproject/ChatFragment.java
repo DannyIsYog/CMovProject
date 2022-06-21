b4d5ade8ca6483cac0f791cbd11482a297993096
urbano2000 <beatriz.urbano@tecnico.ulisboa.pt>
2022-06-21 23:33:25 +0100

    Started implementing showing all chatrooms available

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/ChatFragment.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/ChatFragment.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -38,7 +40,9 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
     private final Type type = Types.newParameterizedType(List.class, Room.class);
     private final JsonAdapter<List<Room>> adapter = moshi.adapter(type);
 
-    private CardView groupChatName;
+    //private CardView groupChatName;
+
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
@@ -52,6 +56,10 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
                 .post(formBody)
                 .build();
 
+
+        View rootView = inflater.inflate(R.layout.fragment_chat,container,false);
+
+
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
@@ -62,14 +70,20 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 String resp = response.body().string();
                 Log.d("Response", resp);
-                List rooms = adapter.fromJson(resp);
+                List<Room> rooms = adapter.fromJson(resp);
                 Log.d("Response", String.valueOf(rooms));
+
+                //create recycleview
+                recyclerView = rootView.findViewById(R.id.recycler);
+                recyclerView .setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+                recyclerView.setAdapter(new GroupListAdapter(rooms));
             }
         });
-        View rootView = inflater.inflate(R.layout.fragment_chat,container,false);
 
-        groupChatName = (CardView) rootView.findViewById(R.id.chatGroup);
-        groupChatName.setOnClickListener(this);
+        //groupChatName = (CardView) rootView.findViewById(R.id.chatGroup);
+        //groupChatName.setOnClickListener(this);
+
 
         return  rootView;
     }
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/RecyclerViewHolder.java
@@ -0,0 +1,20 @@
+package pt.ulisboa.tecnico.cmov.cmovproject;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView view;
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        view = itemView.findViewById(R.id.chatName);
+    }
+
+    public TextView getView(){
+        return view;
+    }
+}
