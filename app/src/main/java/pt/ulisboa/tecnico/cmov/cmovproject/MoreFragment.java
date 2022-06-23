167025a7d53479b9a4e08979422bb912d6f5ef01
urbano2000 <beatriz.urbano@tecnico.ulisboa.pt>
2022-06-23 21:01:38 +0100

    Added some more implementation to create the group

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/MoreFragment.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovproject/MoreFragment.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.cmov.cmovproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,10 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 
 public class MoreFragment extends Fragment implements View.OnClickListener{
@@ -26,10 +31,70 @@ public class MoreFragment extends Fragment implements View.OnClickListener{
 
         View rootView = inflater.inflate(R.layout.fragment_more,container,false);
 
-       
+        btnNewGroup = rootView.findViewById(R.id.btn_createGroup);
+        btnJoinGroup = rootView.findViewById(R.id.btn_joinGroup);
+
+
+        btnNewGroup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addGroup();
+
+            }
+        });
+
+        btnJoinGroup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                joinGroup();
+
+            }
+        });
 
 
         return  rootView;
+    }
+
+    public void addGroup(){
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+        alert.setTitle("Please fill all the fields");
+
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Set an EditText view to get user input
+        final EditText groupName = new EditText(getActivity());
+        groupName.setHint("Name:");
+        layout.addView(groupName);
+        final EditText groupType = new EditText(getActivity());
+        groupType.setHint("Type:");
+        layout.addView(groupType);
+
+        alert.setView(layout);
+
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String name = groupName.getText().toString();
+                String type = groupType.getText().toString();
+
+                // Do something with value!
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+
+    }
+
+    public void joinGroup(){
+        //mostrar os grupos para escolher
+
     }
 
 
